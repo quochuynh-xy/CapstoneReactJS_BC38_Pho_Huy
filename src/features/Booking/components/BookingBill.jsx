03873cf2a16bb03f133bec4aeb92355b2e988a63
huynhquocpho <huynhquocpho@gmail.com>
2023-03-03 19:54:05 +0700

    Mar-03-2023: Tao layout trang chon ghe

--- src/features/Booking/components/BookingBill.jsx
+++ src/features/Booking/components/BookingBill.jsx
@@ -5,26 +5,45 @@ const BookingBill = () => {
   const selectedShow = useSelector((state) => state.booking.selectedShow);
   const thongTinPhim = selectedShow.thongTinPhim;
   return (
-    <div className="bill border border-solid border-green-500 p-2">
-      <h2 className="title">Giỏ hàng của bạn</h2>
-      <p className="bill__moviename">{thongTinPhim?.tenPhim}.</p>
-      <div className="flex flex-wrap justify-between">
-        <p className="basis-full">Adult-Standar-2D:</p>
-        <span className="basis-4/12">Số lượng:</span>
-        <span className="font-bold text-green-400">{orderInfo?.countCommon}</span>
-        <span className="text-orange-400">70.000đ</span>
+    <div className="bill h-fit border border-solid border-slate-100">
+      <div className="title text-center">
+        <h2>Giỏ hàng của bạn</h2>
       </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="basis-full">Adult-VIP-2D:</p>
-        <span className="basis-4/12">Số lượng:</span>
-        <span className="font-bold text-green-400">{orderInfo?.countVip}</span>
-        <span className="text-orange-400">200.000đ</span>
+      <p className="bill__moviename">{thongTinPhim?.tenPhim}</p>
+      <p className="text-xs ml-3 text-slate-400 italic">{thongTinPhim?.tenCumRap +", "+ thongTinPhim?.tenRap}</p>
+      <div className="flex flex-wrap justify-end items-center">
+        <p className="basis-full ticket-type mt-2 pl-3">
+          Adult-Standar-2D:
+        </p>
+        <span className="mr-4 font-semibold text-slate-200">Số lượng:</span>
+        <span className="w-4 font-bold text-green-400 text-center">
+          {orderInfo?.countCommon}
+        </span>
+        <span className="basis-3/12 text-orange-400 ml-2 mr-4 text-end">
+          70.000đ
+        </span>
       </div>
-      <hr />
-      <p>
-        TỔNG CỘNG:
-        <span className="font-medium ml-3 text-orange-500"> 270.000đ</span>
-      </p>
+      <div className="flex flex-wrap justify-end items-center">
+        <p className="basis-full ticket-type mt-2 pl-3">
+          Adult-VIP-2D:
+        </p>
+        <span className="mr-4 font-semibold text-slate-200">Số lượng:</span>
+        <span className="w-4 font-bold text-green-400 text-center">
+          {orderInfo?.countVip}
+        </span>
+        <span className="basis-3/12 text-orange-400 ml-2 mr-4 text-end">
+          200.000đ
+        </span>
+      </div>
+      <div className="selected pl-3 pt-8 font-bold text-slate-200">
+        <p className="seat-name">Ghế đã chọn: <span className="text-red-500 ml-4">H11, H12, E11, E12, F31</span></p>
+      </div>
+      <div className="mt-8 py-4 flex border-t-2 border-solid border-green-500">
+        <p className="text-orange-400 text-lg pl-3">
+          TỔNG CỘNG:
+        </p>
+          <span className="font-medium text-lg ml-auto text-orange-400 mr-4"> 270.000đ</span>
+      </div>
     </div>
   );
 };
--- src/features/Booking/components/BookingSeatsMap.jsx
+++ src/features/Booking/components/BookingSeatsMap.jsx
@@ -2,37 +2,36 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import BookingBill from "./BookingBill";
 // Đang dính bug khi chưa load xong dữ liệu mà nhấn ngay tới bước này thì sinh lỗi.
+// Bug tự nhảy đến trang chọn ghế mà chưa qua trang chọn số lượng vé
 const BookingSeatsMap = () => {
   const listOfseats = useSelector(
     (state) => state.booking.selectedShow.danhSachGhe
   );
   const [seatingChart, setSeatingChart] = useState([]);
   const CommonSeat = (props) => (
-    <button className="p-2 m-1 rounded bg-green-200">
-      {props.data?.soGhe}
-    </button>
+    <button className="common seat">{props.data?.soGhe}</button>
   );
   const VipSeat = (props) => (
-    <button className="p-2 m-1 rounded bg-red-200">{props.data?.soGhe}</button>
+    <button className="vip seat">{props.data?.soGhe}</button>
   );
   const TakenSeat = (props) => (
-    <button className="p-2 m-1 rounded bg-slate-400">
+    <button className="taken seat cursor-not-allowed">
       {props.data?.soGhe}
     </button>
   );
   const splitRow = (totalSeat, quantity = 16) => {
-    if(!totalSeat) return
+    if (!totalSeat) return;
     let character = "ABCDEFGHIJKLMNOP".split("");
     let resut = [];
     let rowName = 0;
-    for (let i = 0; i < totalSeat.length; i += quantity) { 
+    for (let i = 0; i < totalSeat.length; i += quantity) {
       let item = {
         index: character[rowName],
         row: totalSeat.slice(i, i + quantity),
       };
       rowName++;
       item.row = item.row.map((slot, index) => {
-        return { ...slot, soGhe: index+1 };
+        return { ...slot, soGhe: index + 1 };
       });
       resut.push(item);
     }
@@ -40,29 +39,54 @@ const BookingSeatsMap = () => {
   };
   // console.log(splitRow(listOfseats, 16));
   useEffect(() => {
-    if(!listOfseats) return
+    if (!listOfseats) return;
     setSeatingChart(splitRow(listOfseats, 16));
   }, [listOfseats]);
   // Hiện tại dữ liệu trả về không có sơ đồ bố trí nên mặc định sẽ có 16 ghế/hàng
   return (
     <section className="seatandbill pt-20 flex justify-between container mx-auto">
       <div className="seatsMap basis-8/12">
-        {seatingChart.length &&
-          seatingChart.map((singleRow)=>{
-            return <div key={singleRow.index}>
-              {singleRow.row.map(seat => {
-                if(seat.loaiGhe ==="Thuong") {
-                  if(seat.daDat) {return <TakenSeat key={seat.maGhe}/>} else {
-                    return <CommonSeat data={seat} key={seat.maGhe}/>
-                  }
-                } else {
-                  if(seat.daDat) {return <TakenSeat key={seat.maGhe}/>} else {
-                    return <VipSeat data={seat} key={seat.maGhe}/>
-                  }
-                }
-              })}
+        <div className="screen text-center">
+          <p className="font-semibold text-white">MÀN HÌNH</p>
+        </div>
+        <div className="seats">
+          {seatingChart.length &&
+            seatingChart.map((singleRow) => {
+              return (
+                <div key={singleRow.index} className="flex justify-around">
+                  {singleRow.row.map((seat) => {
+                    if (seat.loaiGhe === "Thuong") {
+                      if (seat.daDat) {
+                        return <TakenSeat data={seat} key={seat.maGhe} />;
+                      } else {
+                        return <CommonSeat data={seat} key={seat.maGhe} />;
+                      }
+                    } else {
+                      if (seat.daDat) {
+                        return <TakenSeat data={seat} key={seat.maGhe} />;
+                      } else {
+                        return <VipSeat data={seat} key={seat.maGhe} />;
+                      }
+                    }
+                  })}
+                </div>
+              );
+            })}
+          <div className="note flex mt-3 h-fit flex-wrap pb-6">
+            <div className="flex mr-4 items-center">
+              <span className="common"></span><p className="text-white ml-4">: Ghế thường.</p>
             </div>
-          })}
+            <div className="flex mr-4 items-center">
+            <span className="vip"></span><p className="text-white ml-4">: Ghế VIP.</p>
+            </div>
+            <div className="flex mr-4 items-center">
+            <span className="taken"></span><p className="text-white ml-4">: Ghế đã đặt.</p>
+            </div>
+            <div className="flex mr-4 items-center">
+            <span className="pending"></span><p className="text-white ml-4">: Ghế đang chọn.</p>
+            </div>
+          </div>
+        </div>
       </div>
       <BookingBill />
     </section>
