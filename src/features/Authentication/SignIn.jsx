c236a103c2a0752055a0dace79957b8eff5c973b
huynhquocpho <huynhquocpho@gmail.com>
2023-03-06 22:21:16 +0700

    Mar-06-2023: Create form login-singup

--- src/features/Authentication/SignIn.jsx
+++ src/features/Authentication/SignIn.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginAction } from "./thunk";
 import PageLayout from "../../HOCs/PageLayout";
-import Header from "../../components/Header";
 import { useNavigate } from "react-router-dom";
+import { MdMovieFilter } from "react-icons/md";
 function SignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const isLogin = useSelector( state => state.auth.userLogin);
+  const isLogin = useSelector((state) => state.auth.userLogin);
   const [loginInfo, setLoginInfo] = useState({ taiKhoan: "", matKhau: "" });
   const handleChange = (e) => {
     setLoginInfo({
@@ -18,55 +18,79 @@ function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     await dispatch(loginAction(loginInfo)); // gửi thông tin đăng nhập
-    setLoginInfo({taiKhoan: "", matKhau: "" }) // clear form
-    navigate("/") // chuyển về trang chủ
+    setLoginInfo({ taiKhoan: "", matKhau: "" }); // clear form
+    navigate("/"); // chuyển về trang chủ
   };
-  useEffect(()=> {
-    !isLogin? navigate("/Authentication/SignIn") : navigate("/")
-  },[isLogin, navigate])
+  useEffect(() => {
+    !isLogin ? navigate("/Authentication/SignIn") : navigate("/");
+  }, [isLogin, navigate]);
   return (
     <PageLayout>
-      <Header/>
-      <div className="login container mx-auto mt-10 flex items-center justify-center">
-        <form action="">
-          <h2 className="text-center text-3xl mb-4 text-orange-600 font-bold">
-            Đăng nhập
-          </h2>
-          <div>
-            <label htmlFor="userid">Tài khoản</label>
-            <br />
-            <input
-              className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
-              id="userid"
-              type="text"
-              placeholder="Tài khoản"
-              name="taiKhoan"
-              onChange={handleChange}
-              value={loginInfo.taiKhoan}
-            />
-          </div>
-          <div className="mt-2">
-            <label htmlFor="password">Mật khẩu</label>
-            <br />
-            <input
-              className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
-              id="password"
-              type="password"
-              placeholder="Tài khoản"
-              name="matKhau"
-              onChange={handleChange}
-              value={loginInfo.matKhau}
-            />
-          </div>
-          <div className="text-end">
-            <button
-              className="mr-2 py-2 px-4 bg-red-500 mt-4 rounded-lg text-white font-semibold"
-              onClick={handleSubmit}
-            >
-              Đăng Nhập
-            </button>
-          </div>
-        </form>
+      <div className="login__bgcover">
+        <div className="login container mx-auto flex items-center justify-center">
+          <form
+            action=""
+            className="login__form border border-solid border-red-400 rounded"
+          >
+            <div className="form__header text-center">
+              <div className="text-center">
+                <button className="logo">
+                  <MdMovieFilter className="inline-block" />
+                  <span>tixket box</span>
+                </button>
+              </div>
+              <h2 className="text-center text-xl mb-4 text-orange-600 font-bold">
+                Đăng nhập
+              </h2>
+            </div>
+            <div className="form__body">
+              <div>
+                <label htmlFor="userid">Tài khoản</label>
+                <br />
+                <input
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  id="userid"
+                  type="text"
+                  placeholder="Tài khoản"
+                  name="taiKhoan"
+                  onChange={handleChange}
+                  value={loginInfo.taiKhoan}
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="password">Mật khẩu</label>
+                <br />
+                <input
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  id="password"
+                  type="password"
+                  placeholder="Tài khoản"
+                  name="matKhau"
+                  onChange={handleChange}
+                  value={loginInfo.matKhau}
+                />
+              </div>
+              <div className="text-end">
+                <button
+                  className="signin-btn py-2 px-4 mt-4 rounded-lg text-white font-semibold"
+                  onClick={handleSubmit}
+                >
+                  Đăng Nhập
+                </button>
+              </div>
+            </div>
+            <div className="login__footer mt-4">
+              <p>Chưa có tài khoản Tix?
+              <span
+                className="cursor-pointer ml-1"
+                onClick={() => navigate("/Authentication/SignUp")}
+              >
+                Đăng ký ngay
+              </span>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </PageLayout>
   );
--- src/features/Authentication/SignUp.jsx
+++ src/features/Authentication/SignUp.jsx
@@ -1,6 +1,111 @@
+import { useNavigate } from "react-router-dom";
+import PageLayout from "../../HOCs/PageLayout";
+import { MdMovieFilter } from "react-icons/md";
 function SignUp() {
-  return <div>
-    heyou
-  </div>;
+  const navigate = useNavigate();
+  return (
+    <PageLayout>
+      <div className="signup__bgcover">
+        <div className="signup container mx-auto flex items-center justify-center">
+          <form
+            action=""
+            className="signup__form border border-solid border-red-400 rounded"
+          >
+            <div className="form__header text-center">
+              <div className="text-center">
+                <button className="logo"
+                onClick={()=> navigate("/")}
+                >
+                  <MdMovieFilter className="inline-block" />
+                  <span>tixket box</span>
+                </button>
+              </div>
+              <h2 className="text-center text-xl mb-4 text-orange-600 font-bold">
+                Đăng ký
+              </h2>
+            </div>
+            <div className="form__body">
+              <div>
+                <label htmlFor="hoTen">Họ & tên</label>
+                <br />
+                <input
+                  name="hoTen"
+                  id="hoTen"
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  type="text"
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="taiKhoan">Tài khoản</label>
+                <br />
+                <input
+                  name="taiKhoan"
+                  id="taiKhoan"
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  type="text"
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="matKhau">Mật khẩu</label>
+                <br />
+                <input
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  id="matKhau"
+                  name="matKhau"
+                  type="password"
+                  placeholder=""
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="email">Email</label>
+                <br />
+                <input
+                  name="email"
+                  id="email"
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  type="email"
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="soDt">Số điện thoại</label>
+                <br />
+                <input
+                  id="soDt"
+                  name="soDt"
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  type="text"
+                />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="maNhom">Mã nhóm (GP01-GP10)</label>
+                <br />
+                <input
+                  className="tracking-wider mt-1 pl-2 py-1 text-red-600 font-semibold border-yellow-700 border-2 border-solid rounded-md"
+                  id="maNhom"
+                  name="maNhom"
+                />
+              </div>
+              <div className="text-end">
+                <button className="signin-btn py-2 px-4 mt-4 rounded-lg text-white font-semibold">
+                  Đăng ký
+                </button>
+              </div>
+            </div>
+            <div className="signup__footer mt-4">
+              <p>
+                Đã có tài khoản Tix?
+                <span
+                  className="cursor-pointer ml-2"
+                  onClick={() => navigate("/Authentication/SignIn")}
+                >
+                  Đăng nhập
+                </span>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
 export default SignUp;
